e69cda2318fa1cd870ac1262aa211885e865ee1e
szabolcshlatky <szabolcshlatky@gmail.com>
2024-03-15 20:27:23 +0100

    init: Modern React boilerplate

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './assets/styles/main.scss';
+import App from './default/App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
