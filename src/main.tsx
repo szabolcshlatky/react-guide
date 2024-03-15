8387d9ac502dd95043d51c4ec395cbf00b6fdf4a
szabolcshlatky <szabolcshlatky@gmail.com>
2024-03-15 21:21:42 +0100

    Redux lessons

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/styles/main.scss';
-import { RouterProvider } from 'react-router-dom';
-import router from './routes/routes';
+// import { RouterProvider } from 'react-router-dom';
+// import router from './routes/routes';
+
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import Counter from './features/counter/Counter';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <Counter />
+    </Provider>
   </React.StrictMode>
 );
